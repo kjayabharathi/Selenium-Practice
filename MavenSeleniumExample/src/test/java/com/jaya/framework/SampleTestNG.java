b24f71d344ac5ccaf52b45f42798a85acee84e99
Jaya <jayap.sqa@outlook.com>
2019-12-02 20:42:54 -0600

    Todays updates

--- MavenSeleniumExample/src/test/java/com/jaya/framework/SampleTestNG.java
+++ MavenSeleniumExample/src/test/java/com/jaya/framework/SampleTestNG.java
@@ -3,7 +3,40 @@ package com.jaya.framework;
 import org.testng.annotations.Test;
 
 public class SampleTestNG {
-  @Test
-  public void f() {
-  }
+
+	@Test(priority = 107)
+	public void close() {
+		System.out.println("F");
+	}
+
+	@Test(priority = 106)
+	public void amountVerify() {
+		System.out.println("amountVerify ");
+	}
+
+	@Test(priority = 100, description = " This is the method we are using for login test", enabled = false)
+	public void doLogin() {
+		System.out.println("doLogin ");
+	}
+
+	@Test(dependsOnMethods = "enterUserDetails", enabled = false)
+	public void click() {
+		System.out.println("click ");
+	}
+
+	@Test( description = "Entering username and password", enabled = false)
+	public void enterUserDetails() {
+		System.out.println("enterUserDetails ");
+	}
+
+	@Test(priority = 105)
+	public void moneyTransfer() {
+		System.out.println("moneyTransfer ");
+	}
+
+	@Test
+	public void checkBlalance() {
+		System.out.println("checkBlalance ");
+	}
+
 }
